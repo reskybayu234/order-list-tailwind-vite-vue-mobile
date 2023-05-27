e365734bff554970bd8bf3f6cb8a4c96cb45a7af
Resky Bayu Andhika <bayu@trillium-tech.com>
2023-05-27 22:46:30 +0700

    commit navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <div class="bg-[#002D74] flex justify-between text-white py-3">
+      <div class="ml-5 text-lg">Subarashi Jastiper</div>
+      <div class="bg-gray-300 w-12 rounded-lg mr-5">
+        <div class="bg-[#002D74] rounded h-1 mx-3 my-1"></div>
+        <div class="bg-[#002D74] rounded h-1 mx-3 my-1"></div>
+        <div class="bg-[#002D74] h-1 mx-3 rounded my-1"></div>
+      </div>
+    </div>
+  </section>
+</template>
